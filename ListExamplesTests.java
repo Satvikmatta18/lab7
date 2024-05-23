d343073b79804dda120efacf15d590397ee4ab96
Satvik Matta <smatta@ieng6-201.ucsd.edu>
2024-05-22 22:09:43 -0700

    Fixed index1 to index2, renamed sc to checker, added print statement, and added test for merge on empty lists

--- ListExamplesTests.java
+++ ListExamplesTests.java
@@ -18,5 +18,10 @@ public class ListExamplesTests {
 		List<String> l2 = new ArrayList<String>(Arrays.asList("c", "d", "e"));
 		assertArrayEquals(new String[]{ "a", "b", "c", "c", "d", "e" }, ListExamples.merge(l1, l2).toArray());
         }
-
+	@Test(timeout = 500)
+	public void testmergeEmptyLists(){
+		List<String> l1 = new ArrayList<String>();
+                List<String> l2 = new ArrayList<String>();
+                assertTrue(ListExamples.merge(l1, l1).isEmpty());
+	}
 }
